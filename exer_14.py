6695d67264d4668ca8c456c6cea6f2c9d063fbc2
imVituZeron <vitorpaulasantos@gmail.com>
2021-04-21 22:52:43 -0300

    solution B

--- exer_14.py
+++ exer_14.py
@@ -18,5 +18,25 @@ def numbers_questions(number):
         list_response = [count_yes, count_no]
     return list_response
 
-print(numbers_questions(3))
-        
+# question B
+total_person_list = [] 
+while True:
+    print('Questões')
+    name = input('digite o seu nome: ')
+
+    returno = numbers_questions(3)
+    if returno[0] > returno[1]:
+        person_list = [name,'Yes Group']
+    else:
+        person_list = [name, 'No Group']
+    
+    total_person_list.append(person_list)
+
+    continuE = input('Deseja continuar? [s/n]')
+    if continuE.isalpha():
+        if continuE.upper() == 'N':
+            break
+        else:
+            print('proximo!')
+
+print(total_person_list)
